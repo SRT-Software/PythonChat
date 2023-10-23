308b910b1c6ee6c058827956f9f74060655e6e14
hli <1411186296@qq.com>
2023-10-23 10:52:57 +0800

    feat:add User

--- config/User.py
+++ config/User.py
@@ -1,16 +1,16 @@
 import hashlib
 import os
 
-default_salt = '00000'
+default_salt = '00000'.encode()
 
 
 class User:
     def __init__(self, name, password, salt=default_salt):
         # 使用 SHA-256 和盐值进行加盐哈希加密
         if salt != default_salt:
-            self.salt = salt
-        else:
             self.salt = os.urandom(16)
+        else:
+            self.salt = default_salt
         self.user_name = name
         self.user_password = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 100000).hex()
 
