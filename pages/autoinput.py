1549e10f81b5e85e9def7a875197a31084e4874a
chl <3102727009@qq.com>
2023-10-29 22:50:57 +0800

    streamlit_bokeh

--- pages/autoinput.py
+++ pages/autoinput.py
@@ -1,20 +1,60 @@
 import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
 st.set_page_config(
     page_title="Streamlit语音输入",
     page_icon="🎤"
 )
-# 接收并显示语音输入文本
-text_input = st.text_input("语音输入", value="")
+# # 接收并显示语音输入文本
+# text_input = st.text_input("语音输入", value="")
+#
+# # 在Streamlit中显示语音输入的文本
+# def set_text_input(text):
+#     text_input.text(text)
+#
+# # 通过st.components.html加载HTML文件
+# def load_html_file(file_path):
+#     with open(file_path, "r") as file:
+#         st.components.v1.html(file.read(), width=700, height=500)
+# # 注册Streamlit应用程序的端点，以接收来自JavaScript的POST请求
+#
+# load_html_file("audioInput.html")
+# # 注册用于接收来自JavaScript的POST请求的处理函数
 
-# 在Streamlit中显示语音输入的文本
-def set_text_input(text):
-    text_input.text(text)
+spk_button = Button(label='SPEAK', button_type='success')
+result = streamlit_bokeh_events(
+    bokeh_plot = spk_button,
+    events="GET_TEXT,GET_ONREC,GET_INTRM",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
-# 通过st.components.html加载HTML文件
-def load_html_file(file_path):
-    with open(file_path, "r") as file:
-        st.components.v1.html(file.read(), width=700, height=500)
-# 注册Streamlit应用程序的端点，以接收来自JavaScript的POST请求
+spk_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = true;
+    recognition.lang = 'en';
 
-load_html_file("audioInput.html")
-# 注册用于接收来自JavaScript的POST请求的处理函数
+    var value, value2 = "";
+    for (var i = e.resultIndex; i < e.results.length; ++i) {
+        if (e.results[i].isFinal) {
+            value += e.results[i][0].transcript;
+            rand = Math.random();
+
+        } else {
+            value2 += e.results[i][0].transcript;
+        }
+    }
+    document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: {t:value, s:rand}}));
+    document.dispatchEvent(new CustomEvent("GET_INTRM", {detail: value2}));
+
+    recognition.start();
+    }
+"""))
+
+tr = st.empty()
+if result:
+    if "GET_INTRM" in result:
+        if result.get("GET_INTRM") != '':
+            tr.text_area("**Your input**", result.get("GET_INTRM"))
