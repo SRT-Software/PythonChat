19477e565fb4cbb82fea9336040e47543ac3dd5c
chl <3102727009@qq.com>
2023-10-29 23:46:56 +0800

    bug

--- pages/autoinput.py
+++ pages/autoinput.py
@@ -26,26 +26,41 @@ spk_button = Button(label='SPEAK', button_type='success')
 
 
 spk_button.js_on_event("button_click", CustomJS(code="""
+    var value = "";
+    var rand = 0;
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = false;
     recognition.interimResults = true;
     recognition.lang = 'en';
 
-    var value, value2 = "";
-    for (var i = e.resultIndex; i < e.results.length; ++i) {
-        if (e.results[i].isFinal) {
-            value += e.results[i][0].transcript;
-            rand = Math.random();
-
-        } else {
-            value2 += e.results[i][0].transcript;
-        }
+    document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'start'}));
+    
+    recognition.onspeechstart = function () {
+        document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'running'}));
     }
-    document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: {t:value, s:rand}}));
-    document.dispatchEvent(new CustomEvent("GET_INTRM", {detail: value2}));
+    recognition.onsoundend = function () {
+        document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'stop'}));
+    }
+    recognition.onresult = function (e) {
+        var value2 = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+                rand = Math.random();
+                
+            } else {
+                value2 += e.results[i][0].transcript;
+            }
+        }
+        document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: {t:value, s:rand}}));
+        document.dispatchEvent(new CustomEvent("GET_INTRM", {detail: value2}));
 
+    }
+    recognition.onerror = function(e) {
+        document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'stop'}));
+    }
     recognition.start();
-"""))
+    """))
 
 result = streamlit_bokeh_events(
     bokeh_plot = spk_button,
