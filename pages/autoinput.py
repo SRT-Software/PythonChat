8e5a09d101b16bf7cdaa0b22400f4ebf52d2bf5b
chl <3102727009@qq.com>
2023-10-31 23:02:42 +0800

    chl

--- pages/autoinput.py
+++ pages/autoinput.py
@@ -1,5 +1,3 @@
-from bokeh.io import curdoc
-from bokeh.layouts import column
 from bokeh.models import TextInput, Button, CustomJS
 import streamlit as st
 import streamlit_bokeh_events
@@ -8,22 +6,41 @@ import streamlit_bokeh_events
 text_input = TextInput(value="默认文本", title="文本框标题")
 
 # 创建一个按钮
-button = Button(label="开始语音输入")
+button = Button(label="开始语音输入",button_type ='success')
 
-# 定义回调函数
-text_input_callback = CustomJS(args=dict(text_input=text_input), code="""
+
+
+button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
+    recognition.continuous = false;
     recognition.interimResults = true;
-    recognition.start();
-    recognition.onresult = function(event) {
-        var result = event.results[event.results.length - 1][0].transcript;
-        text_input.value = result;
-    };
-""")
+    recognition.lang = 'en';
+    recognition.onresult = function(e){
+        var value, value2 = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+                rand = Math.random();
+    
+            } else {
+                value2 += e.results[i][0].transcript;
+            }
+        }
+        document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: {t:value, s:rand}}));
+        document.dispatchEvent(new CustomEvent("GET_INTRM", {detail: value2}));
+    }
 
-# 将回调函数绑定到按钮的 onclick 事件上
-button.js_on_event("button_click", text_input_callback)
+    recognition.start();
+    }
+"""))
+
+result = streamlit_bokeh_events(
+    bokeh_plot = button,
+    events="GET_TEXT,GET_ONREC,GET_INTRM",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
 tr = st.empty()
 if result:
